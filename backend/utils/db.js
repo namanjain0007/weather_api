8dd9601cbfb2db78fb48f11fede209abd8774e35
namanjain0007 <namanjainayushjain7@gmail.com>
2025-04-22 12:15:16 +0530

    Update db.js

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDb = async () => {
   try {
+    console.log(process.env.MONGO_URL)
     await mongoose.connect(process.env.MONGO_URL);
+    
     console.log("connection successfull to DB");
   } catch (error) {
     console.log("Databasee connection failed");
