a44e42896cb9bc5b40f4c39a20c1e23ca237061f
namanjain0007 <namanjainayushjain7@gmail.com>
2025-04-22 12:07:35 +0530

    Update auth_controller.js

--- backend/contollers/auth_controller.js
+++ backend/contollers/auth_controller.js
@@ -5,23 +5,22 @@ const fetchWeatherData = async (req, res) => {
   const apiKey = process.env.WEATHER_API_KEY;
 
   try {
-        console.log("Requested City:", city);
+        // console.log("Requested City:", city);
 
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
     );
     const data = await response.json();
-  console.log("response nj",response)
+  // console.log("response nj",response)
     if (response.status === 200) {
       const requiredData = {
         city: data.name,
         temperature: data.main.temp,
         description: data.weather[0].description,
       };
-      console.log("requiredData",requiredData)
-
+      console.log("start")
       await weatherData.create(requiredData);
-
+console.log("end")
       res.status(200).json({ requiredData });
     } else {
       res.status(404).json({ msg: "City not found" });
